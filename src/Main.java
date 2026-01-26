20375df3f41cdd4f9d41f3b9ff392d7f6103d02e
SuperGeorge555 <SuperGeorge555@gmail.com>
2026-01-26 20:45:17 +0300

    add tasks 8.6

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,31 @@ import java.util.Scanner;
 
 public class Main {
     static void main(String[] args) {
-       //8.5
-        Fraction fr1 = new Fraction(4,19);
+       //8.6
+        double sum = sumAll(2,
+                new Fraction(3,5).doubleValue(),
+                2.3);
+        System.out.println("Sum: "+ sum);
 
-        System.out.println(fr1);
-        System.out.println(fr1.intValue());
-        System.out.println(fr1.longValue());
-        System.out.println(fr1.floatValue());
-        System.out.println(fr1.doubleValue());
+        sum = sumAll(3.6,
+                new Fraction(49,12).doubleValue(),
+                3,
+                new Fraction(3,2).doubleValue());
+        System.out.println("Sum: "+ sum);
+
+        sum = sumAll(new Fraction(1,3).doubleValue(),
+                1);
+        System.out.println("Sum: "+ sum);
+    }
+
+    static double sumAll(double ...summands){
+        double sum = 0;
+
+        for(double summand : summands){
+            sum += summand;
+        }
+
+        return sum;
     }
 }
 
