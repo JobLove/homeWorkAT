a11002aea2dc98a515ac20992d955f7aef2f6d29
SuperGeorge555 <SuperGeorge555@gmail.com>
2026-01-28 00:40:01 +0300

    OOP Practice pt3 (exceptions) add tasks 9.3.1

--- src/ru/сourses/main/Main.java
+++ src/ru/сourses/main/Main.java
@@ -4,6 +4,8 @@ import ru.сourses.geometry.PolyLine;
 import ru.сourses.geometry.Point;
 import ru.сourses.geometry.Line;
 
+import java.util.Scanner;
+
 import static java.lang.Integer.parseInt;
 import static java.lang.Math.pow;
 
@@ -11,8 +13,26 @@ import static java.lang.Math.pow;
 public class Main {
     static void main(String[] args) throws Exception {
 
-        Sauce sauce = new Sauce("Острый соус", Spiciness.SPICY);
-        System.out.println(sauce);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите числа через пробел: ");
+        String inputStr = scanner.nextLine();
+        String[] nums = inputStr.split(" ");
+
+        double sum = 0;
+
+        for (String num : nums) {
+            double numDouble;
+            try {
+                numDouble = Double.parseDouble(num);
+            } catch (NumberFormatException e) {
+                numDouble = 0;
+            }
+
+            sum += numDouble;
+        }
+
+        System.out.println("Сумма: " + sum);
 
     }
 
