4a1674e37460bd3725f01ca1d5c50c977accf695
SuperGeorge555 <SuperGeorge555@gmail.com>
2026-01-28 00:25:14 +0300

    OOP Practice pt2 (enum) add tasks 9.2.1

--- src/ru/сourses/main/Main.java
+++ src/ru/сourses/main/Main.java
@@ -11,48 +11,9 @@ import static java.lang.Math.pow;
 public class Main {
     static void main(String[] args) throws Exception {
 
-        //task 9.1.2
-        System.out.println("Solve task 9.1.2:");
-        Point p1 = new Point(1, 2);
-        Point p2 = new Point(1, 2);
-        Point p3 = new Point(1, 3);
-        Point pointClone = p1.clone();
+        Sauce sauce = new Sauce("Острый соус", Spiciness.SPICY);
+        System.out.println(sauce);
 
-        System.out.println(p1.equals(p2));
-        System.out.println(p1.equals(p3));
-        System.out.println(p1.equals(pointClone));
-
-        //task 9.1.3
-        System.out.println();
-        System.out.println("Solve task 9.1.3:");
-        Line l1 = new Line(new Point(1, 2),
-                new Point(2, 3));
-        Line l2 = new Line(1, 2, 2, 3);
-        Line lClone = l1.clone();
-
-        System.out.println(l1.equals(l2));
-        System.out.println(l1.equals(lClone));
-
-        //task 9.1.4
-        System.out.println();
-        System.out.println("Solve task 9.1.4:");
-        PolyLine pl1 = new PolyLine(new Point(1, 2),
-                new Point(2, 3),
-                new Point(3, 4));
-
-        PolyLine pl2 = new PolyLine(new Point(1, 2),
-                new Point(2, 3),
-                new Point(3, 4));
-
-        PolyLine pl3 = new PolyLine(new Point(1, 2),
-                new Point(2, 3),
-                new Point(4, 4));
-
-        PolyLine plClone = pl1.clone();
-
-        System.out.println(pl1.equals(pl2));
-        System.out.println(pl1.equals(pl3));
-        System.out.println(pl1.equals(plClone));
     }
 
     public static double powXY(String x, String y) {
