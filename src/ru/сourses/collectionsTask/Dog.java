f2531f4117b9da657ba60cb62be903f3722e9fb2
SuperGeorge555 <SuperGeorge555@gmail.com>
2026-01-31 17:14:33 +0300

    Reflection API tasks: 10.3.1

---
+++ src/ru/сourses/collectionsTask/Dog.java
@@ -0,0 +1,22 @@
+package ru.сourses.collectionsTask;
+
+public class Dog {
+    public String address;
+    private String name;
+    private int age;
+
+    public Dog(String address, String name, int age) {
+        this.address = address;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "address='" + address + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/ru/сourses/main/Main.java
+++ src/ru/сourses/main/Main.java
@@ -1,23 +1,25 @@
 package ru.сourses.main;
 
 
-
 import ru.сourses.collectionsTask.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 
 public class Main {
     static void main(String[] args) throws Exception {
 
-//10.2.1
-        Stream<String> stream = Stream.of("Hello", "world", "from", "stream");
-        System.out.println("10.2.1: " + Module10.getStringFromStream(stream));
+        //10.3
+        Cat cat = new Cat("Vasya", 10, new ArrayList<>(Arrays.asList("Anton", "Oleg", "Igor")));
+        System.out.println("10.3: " + cat);
+        Module10.resetClass(cat);
+        System.out.println(cat);
 
-        //10.2.2
-        Module10.printList(new ArrayList<>(Arrays.asList(1, 2, 4, 5, 6)));
+        Dog dog = new Dog("Moscow", "Jack", 3);
+        System.out.println(dog);
+        Module10.resetClass(dog);
+        System.out.println(dog);
     }
 }
 
